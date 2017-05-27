3387347a87cde486718e7c765194891357446a19
liaoheng <liaohengcn@gmail.com>
2017-05-27 18:07:40 +0800

    toolbar fix

--- sample/src/main/java/com/github/liaoheng/common/sample/ToolBarActivity.java
+++ sample/src/main/java/com/github/liaoheng/common/sample/ToolBarActivity.java
@@ -2,6 +2,8 @@ package com.github.liaoheng.common.sample;
 
 import android.os.Bundle;
 import android.support.annotation.Nullable;
+import android.view.Menu;
+
 import com.github.liaoheng.common.ui.base.CUBaseActivity;
 
 /**
@@ -9,7 +11,8 @@ import com.github.liaoheng.common.ui.base.CUBaseActivity;
  * @version 2017-01-22 11:45
  */
 public class ToolBarActivity extends CUBaseActivity {
-    @Override protected void onCreate(@Nullable Bundle savedInstanceState) {
+    @Override
+    protected void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_toolbar);
         //initToolBar();
@@ -21,5 +24,11 @@ public class ToolBarActivity extends CUBaseActivity {
 
         getToolBarUtils().getToolbarLeftDef().setImageResource(R.drawable.lcu_ic_close_white_24dp);
         getToolBarUtils().getToolbarRightDef().setImageResource(R.drawable.lcu_ic_drawer_home);
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        getMenuInflater().inflate(R.menu.main, menu);
+        return super.onCreateOptionsMenu(menu);
     }
 }
