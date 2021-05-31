7f1cc841be6d0b62060880fbc78735b71c99fe5a
liaoheng <liaohengcn@gmail.com>
2021-05-31 17:53:31 +0800

    add check ColorOS,FuntouchOS,Motorola ROM function

--- common-core/src/main/java/com/github/liaoheng/common/util/ROM.java
+++ common-core/src/main/java/com/github/liaoheng/common/util/ROM.java
@@ -13,17 +13,22 @@ import java.lang.reflect.Field;
 /**
  * @author liaoheng
  * @version 2018-05-30 11:40
- * @see <a href="https://www.jianshu.com/p/ba9347a5a05a">jianshu</a>
- * @see <a href="https://stackoverflow.com/questions/60122037/how-can-i-detect-samsung-one-ui">oneUi</a>
+ * @see <a href="https://www.jianshu.com/p/ba9347a5a05a">base</a>
+ * @see <a href="https://stackoverflow.com/questions/60122037/how-can-i-detect-samsung-one-ui">OneUI</a>
+ * @see <a href="https://github.com/SenhLinsh/Android-ROM-Identifier/blob/master/rom/src/main/java/com/linsh/rom/BuildPropKeyList.java">ColorOS & FuntouchOS</a>
  */
 public class ROM {
     private static final String TAG = ROM.class.getSimpleName();
 
-    public static final String ROM_FLYME = "FLYME";
+    private static final String ROM_FLYME = "FLYME";
+    private static final String ROM_MOTOROLA = "MOTOROLA";
     private static final String KEY_VERSION_MIUI = "ro.miui.ui.version.name";
     private static final String KEY_VERSION_EMUI = "ro.build.version.emui";
     private static final String KEY_VERSION_OPPO = "ro.build.version.opporom";
     private static final String KEY_VERSION_VIVO = "ro.vivo.os.version";
+    private static final String KEY_VERSION_COLOROS = "ro.rom.different.version";
+    private static final String KEY_VERSION_FUNTOUCHOS = "ro.vivo.os.version";
+
     private static ROM ROM;
 
     public static ROM getROM() {
@@ -31,21 +36,27 @@ public class ROM {
             String version;
             String name = Build.MANUFACTURER;
             if (!TextUtils.isEmpty(version = getProp(KEY_VERSION_MIUI))) {
-                ROM = createMIUI(name, version);
+                ROM = new ROM(MIUI, name, version);
             } else if (!TextUtils.isEmpty(version = getProp(KEY_VERSION_EMUI))) {
-                ROM = createEMUI(name, version);
+                ROM = new ROM(EMUI, name, version);
             } else if (!TextUtils.isEmpty(version = getProp(KEY_VERSION_OPPO))) {
-                ROM = createOPPO(name, version);
+                ROM = new ROM(OPPO, name, version);
             } else if (!TextUtils.isEmpty(version = getProp(KEY_VERSION_VIVO))) {
-                ROM = createVIVO(name, version);
+                ROM = new ROM(VIVO, name, version);
+            } else if (!TextUtils.isEmpty(version = getProp(KEY_VERSION_COLOROS))) {
+                ROM = new ROM(COLOROS, name, version);
+            } else if (!TextUtils.isEmpty(version = getProp(KEY_VERSION_FUNTOUCHOS))) {
+                ROM = new ROM(FUNTOUCHOS, name, version);
             } else {
                 version = Build.DISPLAY;
                 if (version.toUpperCase().contains(ROM_FLYME)) {
-                    ROM = createFLYME(name, version);
+                    ROM = new ROM(FLYME, name, version);
                 } else if (isOneUiSystem()) {
-                    ROM = createOneUI(name, getOneUiVersion());
+                    ROM = new ROM(ONEUI, name, version);
+                } else if (name.toUpperCase().contains(ROM_MOTOROLA) || Build.BRAND.toUpperCase().contains(ROM_MOTOROLA)) {
+                    ROM = new ROM(MOTOROLA, name, version);
                 } else {
-                    ROM = createOTHER(name, version);
+                    ROM = new ROM(OTHER, name, version);
                 }
             }
             L.alog().d(TAG, "getROM : " + ROM);
@@ -73,10 +84,13 @@ public class ROM {
     private static final int VIVO = 4;
     private static final int FLYME = 5;
     private static final int ONEUI = 6;
+    private static final int COLOROS = 7;
+    private static final int FUNTOUCHOS = 8;
+    private static final int MOTOROLA = 9;
     private static final int OTHER = 0;
-    private int rom;
-    private String name;
-    private String version;
+    private final int rom;
+    private final String name;
+    private final String version;
 
     public ROM(int rom, String name, String version) {
         this.rom = rom;
@@ -106,54 +120,44 @@ public class ROM {
                 '}';
     }
 
-    public static ROM createMIUI(String name, String version) {
-        return new ROM(MIUI, name, version);
-    }
-
-    public static ROM createEMUI(String name, String version) {
-        return new ROM(EMUI, name, version);
-    }
-
-    public static ROM createOPPO(String name, String version) {
-        return new ROM(OPPO, name, version);
-    }
-
-    public static ROM createVIVO(String name, String version) {
-        return new ROM(VIVO, name, version);
-    }
-
-    public static ROM createFLYME(String name, String version) {
-        return new ROM(FLYME, name, version);
-    }
-
-    public static ROM createOneUI(String name, String version) {
-        return new ROM(ONEUI, name, version);
-    }
-
-    public static ROM createOTHER(String name, String version) {
-        return new ROM(OTHER, name, version);
-    }
-
     public boolean check(int rom) {
         return getROM().rom == rom;
     }
 
-    public boolean check(String name) {
-        return name.equalsIgnoreCase(getROM().name);
+    public boolean isEmui() {
+        return check(EMUI);
     }
 
-    public boolean isEmui() { return check(EMUI); }
+    public boolean isMiui() {
+        return check(MIUI);
+    }
 
-    public boolean isMiui() { return check(MIUI); }
+    public boolean isVivo() {
+        return check(VIVO);
+    }
 
-    public boolean isVivo() { return check(VIVO); }
+    public boolean isOppo() {
+        return check(OPPO);
+    }
 
-    public boolean isOppo() { return check(OPPO); }
-
-    public boolean isFlyme() { return check(FLYME); }
+    public boolean isFlyme() {
+        return check(FLYME);
+    }
 
     public boolean isOneUi() {
         return check(ONEUI);
+    }
+
+    public boolean isColorOS() {
+        return check(COLOROS);
+    }
+
+    public boolean isFuntouchOS() {
+        return check(FUNTOUCHOS);
+    }
+
+    public boolean isMotorola() {
+        return check(MOTOROLA);
     }
 
     private static boolean isOneUiSystem() {
@@ -170,7 +174,7 @@ public class ROM {
     }
 
     //https://stackoverflow.com/questions/60122037/how-can-i-detect-samsung-one-ui
-    private static String getOneUiVersion() {
+    public static String getOneUiVersion() {
         try {
             int fieldInt = getSemPlatform();
             int version = fieldInt - 90000;
